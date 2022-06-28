01a2af099f2eef430c1331761016e4f0035f6c78
alice29shevchuk <Sevc_sd76@student.itstep.org>
2022-06-28 18:58:44 +0300

    multisent messanges well done

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Client
 {
@@ -6,7 +9,28 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string IP = "127.0.0.1";
+            const int PORT = 8008;
+            var endPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(endPoint);
+                while (true)
+                {
+                    Console.WriteLine("Введите текст: ");
+                    string mes = Console.ReadLine();
+                    var buffer = Encoding.Unicode.GetBytes(mes);
+                    socket.Send(buffer);
+                }
+            }
+            catch { }
+            finally
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            Console.ReadLine();
         }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Server
 {
@@ -6,7 +9,43 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string IP = "127.0.0.1";
+            const int PORT = 8008;
+            var endPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Socket clientSocket = null;
+            try
+            {
+                socket.Bind(endPoint);
+                socket.Listen(10);
+
+
+                clientSocket = socket.Accept();
+                while (true)
+                {
+                    int byteCount = 0;
+                    byte[] buffer = new byte[256];
+                    StringBuilder stringBuilder = new StringBuilder();
+                    do
+                    {
+                        byteCount = clientSocket.Receive(buffer);
+                        stringBuilder.Append(Encoding.Unicode.GetString(buffer, 0, byteCount));
+                    } while (clientSocket.Available > 0);
+
+                    Console.WriteLine($"Client msg:\t{stringBuilder.ToString()}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            Console.ReadKey();
         }
     }
 }
